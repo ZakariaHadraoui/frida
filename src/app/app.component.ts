48db9f073c4b37624b612eef714ff74bff63ea08
zakaria hadraoui <zikoxo05@gmail.com>
2024-11-12 10:12:23 +0100

    fen a 3chiri

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { Component1Component } from './component1/component1.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,SidebarComponent,Component1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
