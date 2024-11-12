5a00f081a435f7e1c4bc7d18683827dc0774a72b
mohamedsellak12 <mohamedsellak597@gmail.com>
2024-11-13 00:39:36 +0100

    mohamed add the home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { Component1Component } from './component1/component1.component';
+import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,SidebarComponent,Component1Component],
+  imports: [RouterOutlet,NavbarComponent,SidebarComponent,Component1Component ,HomeComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:HomeComponent,
+        title:'home'
+    },
+    
+];
