cef09a3a834119a3b36c438d96e482b47e4167f4
mohamedsellak12 <mohamedsellak597@gmail.com>
2024-11-14 09:55:59 +0100

    I just added the setting component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,22 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 import { ShopComponent } from './shop/shop.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
     {
         path: '',
         component:HomeComponent,
-        title:'home'
+        title:'Home'
     },
     {
         path:'shop',
         component:ShopComponent,
         title:'Shop'
+    },{
+        path:'settings',
+        component:SettingsComponent,
+        title:'Settings'
     }
     
 ];
