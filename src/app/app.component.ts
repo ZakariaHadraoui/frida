3a5ddb545fa39d090daa50a9fbaf45dae9c36af3
mohamedsellak12 <mohamedsellak597@gmail.com>
2024-11-15 17:56:35 +0100

    I just do wahd l3yba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { Component1Component } from './component1/component1.component';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { SettingsComponent } from './settings/settings.component';
+import { RightChatComponent } from './right-chat/right-chat.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterModule,NavbarComponent,SidebarComponent,Component1Component ,HomeComponent ,ShopComponent ,SettingsComponent],
+  imports: [RouterOutlet,RouterModule,NavbarComponent,SidebarComponent,Component1Component ,HomeComponent,RightChatComponent ,ShopComponent ,SettingsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { RightChatComponent } from '../right-chat/right-chat.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,RightChatComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
